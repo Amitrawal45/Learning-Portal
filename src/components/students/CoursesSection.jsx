c8535cd176b82db478005a81c120dd95f6d77dab
Amit Rawal <atitrawal4505@gmail.com>
2025-02-12 14:13:32 +0545

    Designing the hero section ,Navbar,searchbar and profile of companies list

--- src/components/students/CoursesSection.jsx
+++ src/components/students/CoursesSection.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const CoursesSection = () => {
   return (
     <div>
-        <h1>CoursesSection</h1>
-    </div>
-  )
-}
+      <h2 className="text-3xl font medium text-gray-800">Learn from the best</h2>
+      <p>
+        Discover our top-related courses astoss various categories. From coding
+        and design to business and wellness, our courses are crafted to deliver
+        results.
+      </p>
 
-export default CoursesSection
+      <Link
+        to="/course-list"
+        className="text-gray-500 border border-gray-500/30 px-10 py-3 rounded"
+        onClick={() => scrollTo(0, 0)}
+      >
+        Show all courses
+      </Link>
+    </div>
+  );
+};
+
+export default CoursesSection;
